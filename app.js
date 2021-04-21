fedebecc30ebb570d80bacd3d8b5267dea241a9c
Татьяна Гриб <gribmichael1998@gmail.com>
2021-04-22 00:03:53 +0300

    draw skin

--- app.js
+++ app.js
@@ -2,24 +2,19 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
    function welcome () {
-      let shell = document.getElementById('appshell') 
-      shell.classList.add('launcher__box')
-      
-      let shellH1 = document.createElement('h1')
-      shellH1.classList.add('launcher__h1')
-      shellH1.textContent = "Добро пожаловать геймеры в майнкрафт"
-      
-      let shellP = document.createElement('p')
-      shellP.classList.add('launcher__p')
-      shellP.textContent = "Выберите версию игры"
-      
+      let shell = document.getElementById('appshell')
+      let selectVersion = document.getElementById('launcher__select')
+      let version = '2.0.2'
+      selectVersion.addEventListener('change', (event) => {
+         version = event.target.value
+         shellA.href = `./versions/${version}/dist/index.html`
+         console.log(version)
+      })
       let shellA = document.createElement('a')
-      shellA.href = "./versions/2.0.1/dist/index.html"
+      shellA.href = `./versions/${version}/dist/index.html`
       shellA.classList.add('launcher__p')
       shellA.textContent = "Войти в игру"
-      
-      shell.appendChild(shellH1)
-      shell.appendChild(shellP)
+
       shell.appendChild(shellA)
    }
    welcome ()
